36977bebbb7933a88017a48fa35449d7cfb426d0
Mikhail Alexandrov <mikesperanskii@gmail.com>
2025-04-22 20:30:05 +0300

    Переработка TaskList - ренейминг стилей в кебаб-кейсе.

--- src/pages/ToDoPage.vue
+++ src/pages/ToDoPage.vue
@@ -1,7 +1,7 @@
 <template>
   <!--  Основная страница приложения  -->
-  <div class="to_do_page">
-    <div class="to_do_page_head">
+  <div class="todo-page">
+    <div class="todo-page-head">
       <h1>Список задач</h1>
       <Button
         :button-text="dialogVisible ? 'Отменить' : 'Добавить'"
@@ -36,7 +36,7 @@ const {
 </script>
 
 <style scoped>
-.to_do_page {
+.todo-page {
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -44,7 +44,7 @@ const {
   padding: 20px;
 }
 
-.to_do_page_head {
+.todo-page-head {
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
@@ -53,29 +53,29 @@ const {
   max-width: 800px;
 }
 
-.to_do_page_head h1 {
+.todo-page-head h1 {
   font-size: 2rem;
   margin-right: 10px;
 }
 
 @media (max-width: 768px) {
-  .to_do_page_head {
+  .todo-page-head {
     justify-content: center;
     text-align: center;
   }
 
-  .to_do_page_head h1 {
+  .todo-page-head h1 {
     font-size: 1.5rem;
   }
 
-  .to_do_page_head button {
+  .todo-page-head button {
     width: 100%;
     margin-top: 10px;
   }
 }
 
 @media (max-width: 480px) {
-  .to_do_page_head h1 {
+  .todo-page-head h1 {
     font-size: 1.2rem;
   }
 }
